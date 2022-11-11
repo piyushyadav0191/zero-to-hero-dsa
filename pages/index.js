1aa15594040c0f0c19fc77b8520e704187e0e414
piyushyadav0191 <piyushyadav0191@gmail.com>
2022-11-11 20:22:05 +0530

    arrays

---
+++ pages/index.js
@@ -0,0 +1,73 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Arrays from '../components/Arrays'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>ZTH:DSA</title>
+        <meta name="description" content="DSA Questions" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Zero To Hero <a href="/">DSA!</a>
+        </h1>
+
+        <p className={styles.description}>
+          By Piyush Yadav{' '}
+          <code className={styles.code}>Scroll down for Questions</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="/" className={styles.card}>
+            <h2>Usage &rarr;</h2>
+            <p>All DSA IMP Questions Provided.</p>
+          </a>
+
+          <a href="/" className={styles.card}>
+            <h2>Implement &rarr;</h2>
+            <p>Implement in your favourite Language!</p>
+          </a>
+
+          <a
+            href="/"
+            className={styles.card}
+          >
+            <h2>Submition &rarr;</h2>
+            <p>Submit Your code on LeetCode after practising each questions.</p>
+          </a>
+
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.card}
+          >
+            <h2>Logic &rarr;</h2>
+            <p>
+              Write the Logic in Copy which you learned from Youtube
+            </p>
+          </a>
+        </div>
+        <Arrays />
+      </main>
+
+      <footer className={styles.footer}>
+
+        <h4>More will Upload after 1 week</h4>
+        <em>Arrays will have 10 questions more </em>
+        <a
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Made by PY
+
+        </a>
+      </footer>
+    </div>
+  )
+}
