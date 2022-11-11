ce0279068297c5c342331fc159c09c301059c974
piyushyadav0191 <piyushyadav0191@gmail.com>
2022-11-11 21:10:41 +0530

    arrays

--- components/Arrays.jsx
+++ components/Arrays.jsx
@@ -13,39 +13,39 @@ const Arrays = () => {
 				<ul>
 					<li>
 						<h2>	Array insert At end</h2>
-						<span><p>You are given an array arr. The size of the array is given by sizeOfArray. You need to insert an elpent at the end.
+						<span><p>You are given an array arr. The size of the array is given by sizeOfArray. You need to insert an element at the end.
 						</p></span>
 					</li>
 					<li>
 						<h3>	Array insert at index</h3>
-						<span><p>You are given an array arr(0-based index). The size of the array is given by sizeOfArray. You need to insert an elpent at given index and print the modified array.
+						<span><p>You are given an array arr(0-based index). The size of the array is given by sizeOfArray. You need to insert an element at given index and print the modified array.
 						</p></span>
 					</li>
 					<li>
-						<h3>	Get Elpent At Index </h3>
-						<span><p>You are given an array arr(0-based indexing). The size of the array is given by n. You need to get the elpent at index i and return it. If no elpent exists at i then return -1.
+						<h3>	Get element At Index </h3>
+						<span><p>You are given an array arr(0-based indexing). The size of the array is given by n. You need to get the element at index i and return it. If no element exists at i then return -1.
 						</p></span>
 					</li>
 					<li>
 						<h3>Array Update At Index </h3>
-						<span><p>You are given an array arr(0-based indexing). The size of the array is given by n. You need to update an elpent at the given index. The arr[i] of the array is initially set to i+1.
+						<span><p>You are given an array arr(0-based indexing). The size of the array is given by n. You need to update an element at the given index. The arr[i] of the array is initially set to i+1.
 						</p></span>
 					</li>
 					<li>
 						<h3>Array Delete And Shift </h3>
-						<span><p>You are given an array arr(0-based indexing). The size of the array is given by n. You need to delete an elpent at given index and print the modified array. The arr[i] of array is initially set to i+1.
-							Deletion means you need to shift all the elpents after that index to the left by 1 position and set the last elpent as zero.
+						<span><p>You are given an array arr(0-based indexing). The size of the array is given by n. You need to delete an element at given index and print the modified array. The arr[i] of array is initially set to i+1.
+							Deletion means you need to shift all the elements after that index to the left by 1 position and set the last element as zero.
 						</p></span>
 					</li>
 					<li>
 						<h3>Who has the majority? </h3>
-						<span><p>Given an array arr[] of size N and two elpents x and y, use counter variables to find which elpent appears most in the array, x or y. If both elpents have the same frequency, then return the smaller elpent.
-							Note:  We need to return the elpent, not its count.
+						<span><p>Given an array arr[] of size N and two elements x and y, use counter variables to find which element appears most in the array, x or y. If both elements have the same frequency, then return the smaller element.
+							Note:  We need to return the element, not its count.
 						</p></span>
 					</li>
 					<li>
 						<h3>Maximum and Minimum In Array </h3>
-						<span><p>Given an array arr of size n, find maximum and minimum elpents in the array.
+						<span><p>Given an array arr of size n, find maximum and minimum elements in the array.
 						</p></span>
 					</li>
 					<li>
@@ -55,17 +55,17 @@ const Arrays = () => {
 					</li>
 					<li>
 						<h3>	Rotate an Array</h3>
-						<span><p>Given an unsorted array arr[] of size N, rotate it by D elpents in the counter-clockwise direction.
+						<span><p>Given an unsorted array arr[] of size N, rotate it by D elements in the counter-clockwise direction.
 						</p></span>
 					</li>
 					<li>
-						<h3>	Sum of Array Elpents </h3>
-						<span><p>Given an integer array arr of size n, you need to sum the elpents of arr.
+						<h3>	Sum of Array elements </h3>
+						<span><p>Given an integer array arr of size n, you need to sum the elements of arr.
 						</p></span>
 					</li>
 					<li>
 						<h3> Is Array Sorted  </h3>
-						<span><p>Given an array a[ ] of size N. The task is to check if array is sorted or not. A sorted array can either be increasingly sorted or decreasingly sorted. Also consider duplicate elpents to be sorted.
+						<span><p>Given an array a[ ] of size N. The task is to check if array is sorted or not. A sorted array can either be increasingly sorted or decreasingly sorted. Also consider duplicate elements to be sorted.
 						</p></span>
 					</li>
 				</ul>
