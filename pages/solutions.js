bee04b823035692a7d6a3da6332bd7a1d62c7f92
piyushyadav0191 <piyushyadav0191@gmail.com>
2022-11-22 11:27:11 +0530

    arrays solutions

--- pages/solutions.js
+++ pages/solutions.js
@@ -69,15 +69,48 @@ const solutions = () => {
 									<h3 className='pb-2 font-bold'>	Rotate an Array <small className={styles.small}>(easy)</small></h3>
 									<span><p>Given an unsorted array arr[] of size N, rotate it by D elements in the counter-clockwise direction.
 									</p></span>
+									<Frame className={styles.frame} initialContent="<!DOCTYPE html><html><head></head><body><script src='https://gist.github.com/piyushyadav0191/793991dd6733aec07ad07faa633af1e9.js'></script></body></html>" />
 								</li>
 								<li className='pb-4'>
 									<h3 className='pb-2 font-bold'>	Sum of Array elements <small className={styles.small}>(easy)</small></h3>
 									<span><p>Given an integer array arr of size n, you need to sum the elements of arr.
 									</p></span>
+									<Frame className={styles.frame} initialContent="<!DOCTYPE html><html><head></head><body><script src='https://gist.github.com/piyushyadav0191/d2424d4f6ea257584be30177da235e2d.js'></script></body></html>" />
 								</li>
 								<li className='pb-4'>
 									<h3 className='pb-2 font-bold'> Is Array Sorted <small className={styles.small}>(easy)</small>  </h3>
 									<span><p>Given an array a[ ] of size N. The task is to check if array is sorted or not. A sorted array can either be increasingly sorted or decreasingly sorted. Also consider duplicate elements to be sorted.
+									</p></span>
+									<Frame className={styles.frame} initialContent="<!DOCTYPE html><html><head></head><body><script src='https://gist.github.com/piyushyadav0191/df1a9bb4684da24bbb017979cd7131ad.js'></script></body></html>" />
+								</li>
+								<li className='pb-4'>
+									<h3 className='pb-2 font-bold'>Two Sum <small className={styles.small}>(medium)</small>  </h3>
+									<span><p>Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+									</p></span>
+								</li>
+								<li className='pb-4'>
+									<h3 className='pb-2 font-bold'>Maximum Subarray <small className={styles.small}>(medium)</small>  </h3>
+									<span><p>Given an integer array nums, find the subarray which has the largest sum and return its sum.
+									</p></span>
+								</li>
+								<li className='pb-4'>
+									<h3 className='pb-2 font-bold'>Move Zeroes <small className={styles.small}>(medium)</small>  </h3>
+									<span><p>Given an integer array nums,  move all zeros to the end of it while maintaining the relative order of the non-zero elements.
+									</p></span>
+								</li>
+								<li className='pb-4'>
+									<h3 className='pb-2 font-bold'>Contains Duplicate <small className={styles.small}>(medium)</small>  </h3>
+									<span><p>Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+									</p></span>
+								</li>
+								<li className='pb-4'>
+									<h3 className='pb-2 font-bold'>Longest Word <small className={styles.small}>(hard)</small>  </h3>
+									<span><p>Have the function LongestWord ( sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers
+									</p></span>
+								</li>
+								<li className='pb-4'>
+									<h3 className='pb-2 font-bold'>Trapping Rain Water <small className={styles.small}>(hard)</small>  </h3>
+									<span><p>Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 									</p></span>
 								</li>
 							</ul>
